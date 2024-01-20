9133feed4a8df9251bce7796d2f80a6b7a0c105b
Makarov Sergey <sgmakgg@gmail.com>
2024-01-20 17:25:14 +0300

    pick

--- 7-swap/7-swap.ts
+++ 7-swap/7-swap.ts
@@ -1,10 +1,13 @@
-const obj: Record<string, number> ={
+type InitialType = Record<string, number>;
+type ResultType = Record<number, string>;
+
+const obj: InitialType ={
     a: 1,
     b: 2
 }
 
-function swapKeyValue(obj: Record<string, number>): Record<number, string> {
-    let result: Record<number, string> = {};
+function swapKeyValue(obj: InitialType): ResultType {
+    let result: ResultType = {};
     for (let key in obj) {
         const newKey = obj[key];
         result[newKey] = key;
